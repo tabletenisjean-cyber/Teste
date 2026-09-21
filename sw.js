c5c5138b8c0bf5d05b5fa021b38da04b3da65780
tabletenisjean-cyber <tabletenis_jean@hotmail.com>
2026-09-21 09:12:01 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,16 +1,16 @@
-const CACHE_NAME = 'marcenaria-v2-1-1';
-const APP_SHELL = ['./','./index.html','./manifest.json','./icons/icon-180.png','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE_NAME = 'marcenaria-v3-offline-2026-09-21-01';
+const CORE = ['./', './index.html', './manifest.json', './logo.png', './icons/icon-180.png', './icons/icon-192.png', './icons/icon-512.png'];
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE)).then(()=>self.skipWaiting()));
 });
 self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(()=>self.clients.claim()));
 });
 self.addEventListener('fetch', event => {
   if(event.request.method !== 'GET') return;
-  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
-    const copy = response.clone();
-    caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(()=>{});
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response=>{
+    const copy=response.clone();
+    caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy)).catch(()=>{});
     return response;
-  }).catch(() => caches.match('./index.html'))));
+  }).catch(()=>caches.match('./index.html')));
 });
